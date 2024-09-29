a6bec72bc4cced39ece87cf3012a0d96800009d9
federico Tartarini <federicotartarini@gmail.com>
2024-09-29 15:34:50 +1000

    build: changed base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://github.com/FedericoTartarini',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/youtube-docusaurus-3',
+  baseUrl: '/youtube-docusaurus-3/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
