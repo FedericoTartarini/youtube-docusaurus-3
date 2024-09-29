a51d5b85dc03fa33dbbb019bbd3836fbcfe1ca74
federico Tartarini <federicotartarini@gmail.com>
2024-09-29 15:46:11 +1000

    build: changed search package

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -146,12 +146,20 @@ const config = {
       },
     }),
 
-  plugins: [
+  themes: [
+    // ... Your other themes.
     [
-      require.resolve("@cmfcmf/docusaurus-search-local"),
-      {
-        // Options here
-      },
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        // language: ["en", "zh"],
+        // ```
+      }),
     ],
   ],
 };
